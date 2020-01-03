6d370cdb5270f122b08f34d59285f01b7d605166
adkif <45813955+adkif@users.noreply.github.com>
2020-01-03 10:51:34 +0200

    Add files via upload

---
+++ p5js-temp-Des2046184395937390712.js
@@ -0,0 +1,107 @@
+let winWidth = 600;
+let winHeigth = 600;
+let init = 0;
+let firstDice;
+let secondDice;
+let ply1;
+let ply2;
+let pointStop;
+let button;
+let action;
+
+
+class Dice {
+  constructor(a, x){
+    this.a = a;
+    this.x = x;
+    this.render(ceil(random(6)));
+  }
+  
+  render(point){
+    
+    fill(228, 34, 34);
+    noStroke();
+    rect(winWidth/2 + this.x - this.a/2, winHeigth/2 - this.a/2, this.a, this.a);
+    fill(255);
+    switch(point) {
+      case 1 :  ellipse(winWidth/2 + this.x, winHeigth/2, this.a/8, this.a/8);
+                break;
+                
+      case 2 :  ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                break;
+                
+      case 3 :  ellipse(winWidth/2 + this.x, winHeigth/2, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                break;
+                
+      case 4 :  ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                break;
+                
+      case 5 :  ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x, winHeigth/2, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                break;
+      
+      case 6 :  ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x - this.a/4, winHeigth/2 + this.a/4, this.a/8, this.a/8);
+                ellipse(winWidth/2 + this.x + this.a/4, winHeigth/2 - this.a/4, this.a/8, this.a/8);
+                break;          
+      
+    }
+    return point;
+  }
+  
+  roll(){
+    return this.render(ceil(random(6)));
+  }
+}
+
+
+
+
+function setup() {
+  createCanvas(winWidth, winHeigth);
+  firstDice = new Dice(100, 100);
+  secondDice = new Dice(100, -100);
+  ply1 = ceil(random(6));
+  ply2 = ceil(random(6));
+  pointStop = 6;
+  button = createButton('Lancer');
+  button.mousePressed(play);
+}
+
+function play () {
+  action = 0;
+}
+
+function draw() {
+  background(200);
+  text('Score: '+init, 20, 20);
+  text('Value 1: '+ply1, 20, 40);
+  text('Value 2: '+ply2, 20, 60);
+  if(action === 0) {
+    if((ply1 === pointStop) && (ply2 === pointStop)){}
+    else{
+      ply1 = firstDice.roll();
+      ply2 = secondDice.roll();
+      init = init + 1;
+    }
+    action = action + 1;
+  }else{
+    if((ply1 === pointStop) && (ply2 === pointStop)){
+      text('Finish', winWidth/2, 20);
+    }
+    firstDice.render(ply1);
+    secondDice.render(ply2);
+  }
+}
